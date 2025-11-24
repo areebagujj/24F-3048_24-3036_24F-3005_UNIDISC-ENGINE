116e23f05843f143328b0a260605a2d3b93deecf
Areesha Chaudhry <areeshanazir574@gmail.com>
2025-11-24 21:10:01 +0500

    LogicEngine Module's Header file
    added by 3048

---
+++ LogicEngine.h
@@ -0,0 +1,37 @@
+﻿#ifndef LOGIC_ENGINE_H
+#define LOGIC_ENGINE_H
+#include "Entities.h"
+
+class LogicEngine
+{
+public:
+    LogicEngine
+    (
+        Student* allStudents,
+        int* studentCount,
+        Faculty* allFaculty,
+        int* facultyCount
+    );
+
+    // Rule 1: Student cannot enroll in the same course twice (¬(P ∧ P))
+    bool verifyStudentEnrollmentUniqueness
+    (
+        int studentId,
+        const char* newCourseCode
+    );
+
+    // Rule 2: Faculty cannot be assigned the same course twice
+    bool verifyFacultyAssignmentUniqueness
+    (
+        int facultyId,
+        const char* newCourseCode
+    );
+
+private:
+    Student* students;
+    int* numStudents;
+    Faculty* faculty;
+    int* numFaculty;
+};
+
+#endif // LOGIC_ENGINE_H
