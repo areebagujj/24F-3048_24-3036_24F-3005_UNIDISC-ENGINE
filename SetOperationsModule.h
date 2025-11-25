2bc82a41c87d7330f37afea3b441d7235d3ece69
JaveriaTariq973 <javeriat081@gmail.com>
2025-11-25 03:11:26 -0800

    Add files via upload

---
+++ SetOperationsModule.h
@@ -0,0 +1,45 @@
+﻿#ifndef SET_OPERATIONS_MODULE_H
+#define SET_OPERATIONS_MODULE_H
+
+#include "Entities.h"
+
+class SetOperationsModule
+{
+public:
+    SetOperationsModule
+    (
+        Student* allStudents,
+        int* studentCount,
+        Course* allCourses,
+        int* courseCount
+    );
+
+    // Functionality: Find Students in (Course A) ∩ (Course B)
+    void findStudentsInIntersection
+    (
+        const char* courseCode1,
+        const char* courseCode2
+    );
+
+    // Functionality: Find Students in (Course A) ∪ (Course B)
+    void findStudentsInUnion
+    (
+        const char* courseCode1,
+        const char* courseCode2
+    );
+
+private:
+    Student* students;
+    int* numStudents;
+    Course* courses;
+    int* numCourses;
+
+    // Helper to check if a student is enrolled in a course
+    bool isStudentEnrolled
+    (
+        Student s,
+        const char* targetCode
+    );
+};
+
+#endif // SET_OPERATIONS_MODULE_H
